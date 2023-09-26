b83e7f27764bf8d1b8b7a0560fc146ef9fd8b592
Padma_Priya <116820600+Padma-Priya-A@users.noreply.github.com>
2023-09-26 12:59:32 -0400

    if else

--- 9. Even or Odd.py
+++ 9. Even or Odd.py
@@ -1,3 +1,5 @@
+#get input for a number. And check the number is even or odd. If even print"The given Number is Even".Else print"The given Number is Odd"
+
 Num=int(input("Number:"))
 if(Num%2==0):
     print("The given Number is Even")
