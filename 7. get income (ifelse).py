e4cc22dd387b8995ef3ed6901f1df4f2bb82a11e
Padma_Priya <116820600+Padma-Priya-A@users.noreply.github.com>
2023-09-26 12:50:23 -0400

    if else

--- 7. get income (ifelse).py
+++ 7. get income (ifelse).py
@@ -1,5 +1,7 @@
+#Get input for variable income. if income is lesser than 7000 print scholarship is avaliable. else not eligle for schlorship.
+
 Income=int(input())
-if(Income>7000):
+if(Income<7000):
     print("SCHOLARSHIP IS AVAILABLE")
 else:
     print("NOT ELIGIBLE FOR SCHLORSHIP")
