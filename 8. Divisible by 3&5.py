ec2f55ca668302eaf8e65400315e1a09dc634ddb
Padma_Priya <116820600+Padma-Priya-A@users.noreply.github.com>
2023-09-26 12:57:04 -0400

    if else

--- 8. Divisible by 3&5.py
+++ 8. Divisible by 3&5.py
@@ -1,3 +1,5 @@
+#Get input for a number and check whether it is divisible by both 3 and 5. if yes then print "The Number is divisible by 3 & 5". else The Number is not divisible.
+
 Num=int(input("Number:"))
 if((Num%3==0)&(Num%5==0)):
     print("The Number is divisible by 3 & 5:")
